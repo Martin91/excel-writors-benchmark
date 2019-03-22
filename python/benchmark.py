c34928eb68e06b79dcaa0298d5681160f5bc30e2
zeqin.hong <zeqin.hong@shopee.com>
2019-03-22 21:41:04 +0800

    feat: add python benchmark

---
+++ python/benchmark.py
@@ -0,0 +1,27 @@
+from random import randint
+import time
+
+from pyexcelerate import Workbook
+
+ROWS = 100000
+COLS = 50
+
+def generate_data():
+	data = [[0] * COLS] * ROWS
+	for row in range(ROWS):
+		for col in range(COLS):
+			data[row][col] = randint(1, 10000)
+	return data
+
+def test():
+	begin_timestamp = time.time()
+	wb = Workbook()
+	wb.new_sheet("sheet name", data=generate_data())
+	wb.save("output/python/output.xlsx")
+	end_timestamp = time.time()
+
+	print("Writing 10000x50 cells of data takes %f seconds" % (end_timestamp - begin_timestamp))
+
+
+if __name__ == '__main__':
+	test()
